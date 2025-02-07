025a37232569c861aac51912c38e205fec9103b0
EmilioRivera0 <elcholula619@gmail.com>
2025-02-07 13:16:03 -0600

    Chore: restructured the project files organization

---
+++ src/app/header.tsx
@@ -0,0 +1,5 @@
+export function Header() {
+    return (
+        <section className="border-2">Test</section>
+    );
+}
---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+import { Header } from "./header";
+
+export const metadata: Metadata = {
+  title: "Portfolio",
+  description: "My Professional Portfolio",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <Header />
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+}
